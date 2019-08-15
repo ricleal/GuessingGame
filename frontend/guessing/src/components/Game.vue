e8066577bf573a3ca969d49843bbf667e21b5965
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-08-15 11:04:50 -0400

    axios working in dev

--- frontend/guessing/src/components/Game.vue
+++ frontend/guessing/src/components/Game.vue
@@ -7,27 +7,33 @@
           class="subheading font-weight-regular"
         >To start a game please selected a boundary for guessing a number</p>
       </v-flex>
-
-      <v-flex xs12>
-        <v-row>
-          <v-col cols="12" sm="6" md="4">
-            <v-text-field type="number" label="Minimum" required></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6" md="4">
-            <v-text-field type="number" label="Maximum" required></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6" md="4">
-            <v-btn @click="startGame">Start the Game</v-btn>
-          </v-col>
-        </v-row>
+    </v-layout>
+    <v-layout row justify-space-between>
+      <v-flex md3>
+        <v-text-field type="number" label="Minimum" value="1" min="1" max="50" required></v-text-field>
       </v-flex>
-      
+      <v-flex md3>
+        <v-text-field type="number" label="Maximum" value="50" min="1" max="50" required></v-text-field>
+      </v-flex>
+      <v-flex md3>
+        <v-btn @click="startGame">Start the Game</v-btn>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import Vue from 'vue';
+
 export default {
-  data: () => ({})
+  data: () => ({}),
+  methods: {
+    startGame: () => {
+      Vue.axios.get("/api/start/1/10").then(response => {
+        /* eslint-disable no-console */
+        console.log(response.data);
+      });
+    }
+  }
 };
 </script>
---
+++ frontend/guessing/vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+module.exports = {
+    // options...
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:5000',
+                ws: true,
+                changeOrigin: true
+            },
+        }
+    }
+}
